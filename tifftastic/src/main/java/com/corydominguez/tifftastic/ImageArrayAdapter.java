26ed5e6144cdc03e3d1c40a5a1c9ddfb7c626ad2
Cory Dominguez <corydominguez@gmail.com>
2014-01-14 00:46:17 -0800

    added ImageAdapter class and configured

--- tifftastic/src/main/java/com/corydominguez/tifftastic/ImageArrayAdapter.java
+++ tifftastic/src/main/java/com/corydominguez/tifftastic/ImageArrayAdapter.java
@@ -1,7 +1,11 @@
 package com.corydominguez.tifftastic;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import com.loopj.android.image.SmartImageView;
 
 import java.util.List;
 
@@ -10,6 +14,22 @@ import java.util.List;
  */
 public class ImageArrayAdapter extends ArrayAdapter<Image> {
     public ImageArrayAdapter(Context context, List<Image> images) {
-        super(context, android.R.layout.simple_list_item_1, images);
+        super(context, R.layout.item_image_result, images);
+    }
+
+    @Override
+    public SmartImageView getView(int position, View convertView, ViewGroup parent) {
+        Image image = this.getItem(position);
+        SmartImageView ivImage;
+        if(convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            ivImage = (SmartImageView) inflater.inflate(R.layout.item_image_result, parent, false);
+        } else {
+            ivImage = (SmartImageView) convertView;
+            ivImage.setImageResource(android.R.color.transparent);
+        }
+        ivImage.setImageUrl(image.getTbUrl());
+        return ivImage;
+
     }
 }
